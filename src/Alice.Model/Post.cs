33d889b5621c517059248a94f7a1a17d51a8b902
otakustay <otakustay@gmail.com>
2012-11-20 14:34:02 +0800

    Added Alice.Model project

---
+++ src/Alice.Model/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Alice.Model {
+    public class Post {
+        public string Title { get; set; }
+
+        public string Excerpt { get; set; }
+
+        public string Content { get; set; }
+
+        public string[] Tags { get; set; }
+
+        public DateTime PostDate { get; set; }
+    }
+}
