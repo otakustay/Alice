648abb7eb013082b5be89d1fd285143f866645c5
otakustay <otakustay@gmail.com>
2012-11-20 14:47:49 +0800

    Initialized project structure

---
+++ src/Alice.Web/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Alice.Model;
+
+namespace Alice.Web.Controllers {
+    public class HomeController : Controller {
+        //
+        // GET: /Home/
+
+        [HttpGet]
+        public ActionResult Index() {
+            return View();
+        }
+
+    }
+}
