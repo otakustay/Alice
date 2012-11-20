2643dfc1eb292e82662072b297e6f1f80314eab9
otakustay <otakustay@gmail.com>
2012-11-20 16:46:56 +0800

    Fixed typo

--- res/mysql/create-table-post.sql
+++ res/mysql/create-table-post.sql
@@ -3,7 +3,7 @@ create table `post` (
   `title` varchar(200) not null,
   `content` text not null,
   `excerpt` text not null,
-  `tag` varchar(400) not null,
+  `tags` varchar(400) not null,
   `post_date` datetime not null,
   PRIMARY KEY (`name`)
 ) engine=InnoDB default charset=utf8;
