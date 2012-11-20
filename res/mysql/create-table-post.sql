c4424f361b4dfec328ad62b3830189af4040de5b
otakustay <otakustay@gmail.com>
2012-11-20 15:32:19 +0800

    Added name column for post

--- res/mysql/create-table-post.sql
+++ res/mysql/create-table-post.sql
@@ -1,8 +1,9 @@
 create table `Post` (
+  `Name` varchar(200) not null,
   `Title` varchar(200) not null,
   `Content` text not null,
   `Excerpt` text not null,
   `Tags` varchar(400) not null,
   `PostDate` datetime not null,
-  PRIMARY KEY (`Title`)
+  PRIMARY KEY (`Name`)
 ) engine=InnoDB default charset=utf8;
