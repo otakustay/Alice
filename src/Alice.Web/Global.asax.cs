485f78b7d462b32840baca4c700ef744910efdc1
otakustay <otakustay@gmail.com>
2012-11-21 15:32:52 +0800

    Fixed default route to Post.List

--- src/Alice.Web/Global.asax.cs
+++ src/Alice.Web/Global.asax.cs
@@ -37,7 +37,7 @@ namespace Alice.Web {
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Post", action = "List", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
