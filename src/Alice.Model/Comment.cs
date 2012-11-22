c3e5cf16e70939115753130b249312b246fd6902
otakustay <otakustay@gmail.com>
2012-11-22 15:07:09 +0800

    Added comment model

---
+++ src/Alice.Model/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Alice.Model {
+    public class Comment {
+        public int Id { get; set; }
+
+        public string PostName { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime PostTime { get; set; }
+
+        public CommentAuthor Author { get; set; }
+    }
+}
---
+++ src/Alice.Model/CommentAuthor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Alice.Model {
+    public class CommentAuthor {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string IpAddress { get; set; }
+
+        public string UserAgent { get; set; }
+    }
+}
