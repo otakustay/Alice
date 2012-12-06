0a1155e08997a1c57157b6afdbc5090eb4a44ffd
otakustay <otakustay@gmail.com>
2012-12-06 20:58:04 +0800

    Added ConsoleController

---
+++ src/Alice.Web/Controllers/ConsoleController.cs
@@ -0,0 +1,23 @@
+﻿using MarkdownDeep;
+using NHibernate;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alice.Web.Controllers {
+    public class ConsoleController : Controller {
+        [Inject]
+        public Markdown Transformer { get; set; }
+
+        [Inject]
+        public ISession DbSession { get; set; }
+
+        public ActionResult Index() {
+            return View();
+        }
+
+    }
+}
