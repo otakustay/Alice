5d655ee5b5c4a4bcbcdb6ff496d00e3f454414b9
otakustay <otakustay@gmail.com>
2012-12-06 21:32:00 +0800

    Added file list for console

--- src/Alice.Web/Controllers/ConsoleController.cs
+++ src/Alice.Web/Controllers/ConsoleController.cs
@@ -1,8 +1,10 @@
-﻿using MarkdownDeep;
+﻿using Alice.Model;
+using MarkdownDeep;
 using NHibernate;
 using Ninject;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,8 +18,11 @@ namespace Alice.Web.Controllers {
         public ISession DbSession { get; set; }
 
         public ActionResult Index() {
-            return View();
+            string dir = Server.MapPath("~/Content");
+            IEnumerable<FileInfo> files = new DirectoryInfo(dir)
+                .GetFiles()
+                .OrderByDescending(f => f.LastWriteTime);
+            return View(files);
         }
-
     }
 }
