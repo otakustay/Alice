95b603f85d3fcf24effa67a48dbccb0e3a7e6e34
otakustay <otakustay@live.com>
2013-01-05 13:28:16 +0800

    Minor fix

--- src/Alice.Web/Models/CommentAuditionView.cs
+++ src/Alice.Web/Models/CommentAuditionView.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Web;
 
 namespace Alice.Web.Models {
-    public class CommentAuditionView : CommentView {
+    public class CommentAuditionView : Comment {
         public PostExcerpt Post { get; set; }
     }
 }
