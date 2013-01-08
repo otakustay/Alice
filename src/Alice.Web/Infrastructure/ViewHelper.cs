a237884652f312c15581976ee72651d4735c557e
otakustay <otakustay@live.com>
2013-01-08 14:45:06 +0800

    Change theme by time

---
+++ src/Alice.Web/Infrastructure/ViewHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alice.Web.Infrastructure {
+    public static class ViewHelper {
+        private static Dictionary<string, int> version = new Dictionary<string, int>() {
+            { "site-day.css", 1 }, { "site-night.css", 1 },
+            { "blog-day.css", 1 }, { "blog-night.css", 1 },
+            { "list-day.css", 1 }, { "list-night.css", 1 },
+            { "post-day.css", 1 }, { "post-night.css", 1 },
+            { "console-day.css", 1 }, { "console-night.css", 1 }
+        };
+
+        public static string Style(this UrlHelper url, string name) {
+            int hour = DateTime.Now.Hour;
+            string themeType = (hour >= 6 && hour <= 18) ? "day" : "night";
+            string cssFilename = String.Format("{0}-{1}.css", name, themeType);
+
+            return url.Content("~/styles/" + cssFilename + "?v=" + version[cssFilename]);
+        }
+    }
+}
---
+++ src/Alice.Web/Styles/build.sh
@@ -0,0 +1,9 @@
+﻿for theme in 'day' 'night'
+do
+    echo '@import "theme-'${theme}'.less";' > theme.less
+    for file in 'site' 'blog' 'list' 'post' 'console'
+    do
+        lessc ${file}'.less' ${file}'-'${theme}'.css'
+    done
+done
+rm *.less
