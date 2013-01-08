57a7976e01495a24b6e368040a2e5aefba489f4e
otakustay <otakustay@live.com>
2013-01-08 15:59:53 +0800

    Fixed build.sh

--- src/Alice.Web/Styles/build.sh
+++ src/Alice.Web/Styles/build.sh
@@ -2,7 +2,7 @@
 path=$(dirname $(echo ${0} | sed 's/\\/\//g'))
 for theme in 'night' 'day'
 do
-    echo '@import "theme-'${theme}'.less";' > theme.less
+    echo '@import "theme-'${theme}'.less";' > ${path}/theme.less
     for file in 'site' 'blog' 'list' 'post' 'console'
     do
         lessc ${path}/${file}'.less' ${path}/${file}'-'${theme}'.css'
