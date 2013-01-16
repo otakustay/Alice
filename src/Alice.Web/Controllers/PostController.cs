8420c5c32bee74b8f6a87b0fb2d39a9538787b92
otakustay <otakustay@live.com>
2013-01-16 14:20:39 +0800

    Fixed wrong comment

--- src/Alice.Web/Controllers/PostController.cs
+++ src/Alice.Web/Controllers/PostController.cs
@@ -188,7 +188,7 @@ namespace Alice.Web.Controllers {
             Dictionary<int, string> targetAuthor = new Dictionary<int, string>();
             if (comment.Target.HasValue) {
                 Comment target = DbSession.Get<Comment>(comment.Target);
-                // 防止CFRS攻击，评论只能评论同一文章下的
+                // 防止CSRF攻击，评论只能评论同一文章下的
                 if (target == null || target.PostName != comment.PostName) {
                     comment.Target = null;
                 }
